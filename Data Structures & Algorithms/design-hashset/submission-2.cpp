87bdfcbba47caccab5d151d8ccc8e6618cb3821f
MuVen <muven89@gmail.com>
2026-06-02 13:54:37 +0530

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.cpp
@@ -0,0 +1,31 @@
+class MyHashSet {
+    unordered_set<int> uset;
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        uset.insert(key);
+    }
+    
+    void remove(int key) {
+        auto it = uset.find(key);
+        if(it != uset.end()){
+            uset.erase(it);
+        }
+    }
+    
+    bool contains(int key) {
+        if(uset.find(key) != uset.end()) return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
